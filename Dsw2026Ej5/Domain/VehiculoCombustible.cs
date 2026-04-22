c301f2f491228ecc04fcc534b577bb610e0bfb5a
Martin Madrid <martin.madrid@alu.frt.utn.edu.ar>
2026-04-22 19:29:27 -0300

    Correción de métodos de calcular consumo

--- Dsw2026Ej5/Domain/VehiculoCombustible.cs
+++ Dsw2026Ej5/Domain/VehiculoCombustible.cs
@@ -28,6 +28,15 @@ public class VehiculoCombustible: Vehiculo
 
     public override double CalcularConsumo(double kilometros)
     {
-        return kilometros * kilometrosPorLitro;
+        double consumo = kilometros / kilometrosPorLitro;
+
+        int antiguedad = 2026 - GetAnio();
+
+        if (antiguedad > 5)
+        {
+            consumo += (kilometros / 15.0) * litrosExtra;
+        }
+
+        return consumo;
     }
 }
